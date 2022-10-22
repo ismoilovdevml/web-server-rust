29f91e8f3a29861135f4812baec974b6278cd6dc
ismoilovdev <ismoilovdevarchlinux@gmail.com>
2022-10-22 17:41:46 +0500

    rust server read html

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener,TcpStream},
 };
@@ -20,7 +21,14 @@ fn handle_connection(mut stream: TcpStream){
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // stream.write_all(response.as_bytes()).unwrap();
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let lenght = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-lenght: {lenght}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 
 
