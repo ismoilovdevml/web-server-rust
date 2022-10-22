7ce7cf649ae492b2cb695595761b2aae9d473f84
ismoilovdev <ismoilovdevarchlinux@gmail.com>
2022-10-22 15:07:15 +0500

    update HTTP

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,12 @@ fn main(){
 fn handle_connection(mut stream: TcpStream){
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
-    .lines()
-    .map(|result| result.unwrap())
-    .take_while(|line| !line.is_empty())
-    .collect();
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 
-println!("So'rov: {:#?}", http_request);
+
 }
